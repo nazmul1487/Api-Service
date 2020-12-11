f08a945807cd02255bdda6b381a815d1cd95dd00
nazmul1487 <nazmul15-1487@diu.edu.bd>
2020-12-11 17:26:20 +0600

    [CREATE] Data Process view custom data input view create. Check the token from session,if token authorize is valid then data can input by use. otherwise need to again login.   -assign tsync_id in json data   -request to the API with form data   -headers         -content-type: application/json         -Authorization: Token + auth token   -return the file id to upload files
    Resolves: #4

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import UserLoginView
+from api.views import UserLoginView, DataInput
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
+    path('data/',DataInput.as_view(), name="data"),
 ]
