01d4a0a8f066f839d8e1a56e29d2025faf0d778d
nazmul1487 <nazmul15-1487@diu.edu.bd>
2020-12-11 15:00:27 +0600

    [UPDATED] Login View is updated custom user login view create from function base view  -added static files  -login form is created
    Resolves: #1

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import LoginView
+from api.views import UserLoginView
 
 urlpatterns = [
-    path('login/',LoginView, name="login"),
+    path('login/', UserLoginView.as_view(), name="login"),
 ]
